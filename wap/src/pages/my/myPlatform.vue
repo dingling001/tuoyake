f2b5bd7cdf8488e44e705b0eb32eead9babfbfc1
dingling <dingling>
2019-10-16 19:23:21 +0800

    10.16

--- wap/src/pages/my/myPlatform.vue
+++ wap/src/pages/my/myPlatform.vue
@@ -1,10 +1,28 @@
 <template>
-    
+
 </template>
 
 <script>
     export default {
-        name: "myPlatform"
+        name: "myPlatform",
+        data() {
+            return {
+                platinfo: {}
+            }
+        },
+        created() {
+            this._Platform();
+        },
+        methods: {
+            _Platform() {
+                this.$api.Platform().then(res => {
+                    console.log(res)
+                    if (res.code == 1) {
+                        this.platinfo = res.data;
+                    }
+                })
+            }
+        }
     }
 </script>
 
